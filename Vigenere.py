01a0c76d719e20f4d166f6e9fc5cb30f545ec611
Jason Benner <jbenner1984@gmail.com>
2018-09-22 11:13:34 -0400

    Add files via upload

--- Vigenere.py
+++ Vigenere.py
@@ -70,32 +70,109 @@ def decryptPassage(passage, myAlphabet, myLines, key2string):
     return decryptedPassage
 
 
+def encryptPassage(passage, myAlphabet, myLines, key2string):
+
+    encryptedPassage = []
+
+    counter = 0
+
+    for letter in passage:
+
+        index = myAlphabet.index(letter) - 1
+
+        ourRow = myLines[counter]
+
+        encLetter = ourRow[index]
+
+        encryptedPassage.append(encLetter)
+
+        if counter < len(key2string) - 1:
+            counter += 1
+        else:
+            counter = 0
+
+    return encryptedPassage
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-key1string = input("Enter first key: ")
-key1string = key1string.lower()
 
-myAlphabet = createAlphabet(key1string)
+choiceOfRun = ''
 
-myAlphabetString = ''.join(myAlphabet)
+while choiceOfRun != "A" or "B" or "Q":
 
-print("Your alphabet line is: " + myAlphabetString)
+    choiceOfRun = input("Do you want to: A) Encrypt a passage. B) Decrypt a passage. Q) Quit program [A/B/Q]?").upper()
 
-key2string = input("Enter second key: ")
-key2string = key2string.lower()
+    if choiceOfRun == "A":
 
-myLines = createLines(key2string, myAlphabet)
+        # Encryption
+        key1stringenc = input("Enter first key: ")
+        key1stringenc = key1stringenc.lower().replace(" ", "")
 
-for line in myLines:
+        myAlphabet = createAlphabet(key1stringenc)
 
-    lineSting = ''.join(line)
-    print("Your lines are: " + lineSting)
+        myAlphabetString = ''.join(myAlphabet)
 
-encryptedPassage = input("Enter encrypted passage: ")
-encryptedPassage = encryptedPassage.lower()
+        print("Your alphabet line is: " + myAlphabetString)
 
-decryptedPassage = decryptPassage(encryptedPassage, myAlphabet, myLines, key2string)
+        key2stringenc = input("Enter second key: ")
+        key2stringenc = key2stringenc.lower().replace(" ", "")
 
-decryptedPassageString = ''.join(decryptedPassage)
+        myLines = createLines(key2stringenc, myAlphabet)
 
-print("Your decrypted passage is: " + decryptedPassageString)
+        for line in myLines:
+
+            lineSting = ''.join(line)
+            print("Your lines are: " + lineSting)
+
+        passageInput = input("Enter passage you wish to encrypt: ")
+        passageInput = passageInput.lower().replace(" ", "")
+
+        encryptedPassage = encryptPassage(passageInput, myAlphabet, myLines, key2stringenc)
+
+        encryptedPassageString = ''.join(encryptedPassage)
+
+        print("Your encrypted passage is: " + encryptedPassageString)
+
+        choiceOfRun = ''
+
+        continue
+
+    if choiceOfRun == "B":
+        # Decryption
+        key1string = input("Enter first key: ")
+        key1string = key1string.lower().replace(" ", "")
+
+        myAlphabet = createAlphabet(key1string)
+
+        myAlphabetString = ''.join(myAlphabet)
+
+        print("Your alphabet line is: " + myAlphabetString)
+
+        key2string = input("Enter second key: ")
+        key2string = key2string.lower().replace(" ", "")
+
+        myLines = createLines(key2string, myAlphabet)
+
+        for line in myLines:
+
+            lineSting = ''.join(line)
+            print("Your lines are: " + lineSting)
+
+        encryptedPassageInput = input("Enter passage you wish to decrypt: ")
+        encryptedPassageInput = encryptedPassageInput.lower().replace(" ", "")
+
+        decryptedPassage = decryptPassage(encryptedPassageInput, myAlphabet, myLines, key2string)
+
+        decryptedPassageString = ''.join(decryptedPassage)
+
+        print("Your decrypted passage is: " + decryptedPassageString)
+
+        choiceOfRun = ''
+
+        continue
+
+    if choiceOfRun == "Q":
+        break
+
+
